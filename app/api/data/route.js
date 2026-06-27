bc0e937130adbe4f6c9158a643adfea76120fcd7
yhogakafi <yhogadoang3@gmail.com>
2026-06-27 09:48:04 +0700

    change blob

--- app/api/data/route.js
+++ app/api/data/route.js
@@ -3,6 +3,8 @@ import { getLatestAnalysis } from '@/lib/blob'
 
 export const runtime = 'nodejs'
 export const dynamic = 'force-dynamic'
+export const revalidate = 0
+export const fetchCache = 'force-no-store'
 
 export async function GET() {
   try {
